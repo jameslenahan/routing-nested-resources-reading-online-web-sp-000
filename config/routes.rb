e5153dd26a50643de11b56292b8f65b0cbf71ba0
James Lenahan <james.k.lenahan@gmail.com>
2020-03-13 18:28:08 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :artists do
-    resources :songs, only: [:index, :show]
-  end
-  resources :songs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:show]
+
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  root 'posts#index'
+
 end
